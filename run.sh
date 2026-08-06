b6e6f583be120ee9031a8f981fdaae265c40c92c
hejun <hejun72@outlook.com>
2026-08-06 16:31:53 +0800

    feat: 整理run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,18 @@ set -e
 
 source /usr/local/Ascend/ascend-toolkit/latest/set_env.sh
 
+# 编译推理脚本
 cd atb
 bash build.sh
 
 cd "$(dirname "$0")/models/qwen2.5-0.5b"
 
+# 生成输入
 PYTHONPATH=../../..:.. python3 -m model.prepare_air_inputs --device 8 --prune-lm-head
 
-./run.sh pass
-./run.sh export --prune
-./run.sh atc
-./run.sh bench --profiling --warmup 50 --requests 100 --fixed-seq 150
-./run.sh bench --profiling --warmup 50 --requests 100 --fixed-seq 218
+
+./run.sh pass # 编译安装pass
+./run.sh export --prune # 导出air
+./run.sh atc # 导出om
+./run.sh bench --profiling --warmup 50 --requests 100
 ./run.sh parse
